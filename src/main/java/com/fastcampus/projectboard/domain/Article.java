3310c540508bed756c76f554b75642033ee4550f
jaeheeeKim <jh960902@naver.com>
2024-01-29 15:54:48 +0900

    properties 파일 수정 및 도메인 수정 등
    properties 파일 수정 및 도메인 수정

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,8 +1,22 @@
 package com.fastcampus.projectboard.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
+
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
 public class Article { // ArticleDTO
+    @Id // primarykey
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동으로 오토인크리먼트를 걸어주기 위해서
     private Long id;
     private String title; // 제목
     private String content; // 내용
