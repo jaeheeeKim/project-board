c1011c740f1aeebcf39e42bf92c61f51405465d1
jaeheeeKim <jh960902@naver.com>
2024-04-15 13:32:54 +0900

    #6 - jpa test 작성
    기본 CRUD + 연관관계 매핑과 cascading 이 잘 동작하는지 확인하려고 작성.
    테스트 데이터와 관련이 있기 때문에, 이대로는 실패한다.

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -15,7 +15,7 @@ import java.util.Objects;
 
 @Getter
 @ToString
-@Table(name = "article_comment",
+@Table(
         indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
--- src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 //@ActiveProfiles("testdb")
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)// Auditing을 알려줌
-@DataJpaTest // junit5보다는 assertj를 더 좋아하심
+@DataJpaTest // junit5보다는 assertj를 더 좋아하심 / 이 어노테이션을 적어주면 자동으로 트랜잭션이 걸림! rollback이 디폴트임!
 class JpaRepositoryTest {
 
     private final ArticleRepository articleRepository;
@@ -48,11 +48,8 @@ class JpaRepositoryTest {
     void givenTestData_whenInserting_thenWorksFine() {
         // given
         long previousCount = articleRepository.count();
-
         // when
         Article savedArticle = articleRepository.save(Article.of("new article", "new content", "#spring"));
-
-
         // then
         assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
 
@@ -63,13 +60,31 @@ class JpaRepositoryTest {
     void givenTestData_whenUpdating_thenWorksFine() {
         // given
         Article article = articleRepository.findById(1L).orElseThrow(); // 아이디 1이 없으면 끝내도록
-        article.setHashtag("#springboot");
+        String updatedHashtag = "#springboot";
+        article.setHashtag(updatedHashtag);
 
         // when
-        Article savedArticle = articleRepository.save(Article.of("new article", "new content", "#spring"));
-
+//        Article savedArticle = articleRepository.save(article);
+        Article savedArticle = articleRepository.saveAndFlush(article);
 
         // then
-//        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag",);
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag",updatedHashtag);
+    }
+
+    @DisplayName("delete 테스트")
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine() {
+        // given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long previousArticleCount = articleRepository.count();
+        long previousArticleCommentCount = articleCommentRepository.count();
+        int deletedCommentsSize = article.getArticleComments().size();
+
+        // when
+        articleRepository.delete(article);
+
+        // then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
     }
 }
